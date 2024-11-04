fd1e02fa92a279dd72198c59ff586cb7de06846c
acjr1910 <acjr1910@gmail.com>
2024-11-04 17:27:20 +0000

    add LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,5 @@
 class HTMLNode():
-  def __init__(self, tag=None, value=None, children=None, props=None):
+  def __init__(self, tag=None, value=None, props=[], children=None):
     self.tag = tag
     self.value = value
     self.children = children
@@ -16,3 +16,16 @@ class HTMLNode():
 
   def __repr__(self):
     return f"tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props_to_html()}"
+
+
+class LeafNode(HTMLNode):
+  def __init__(self, tag, value, props=[]):
+    super().__init__(tag, value, props, children=None)
+
+  def to_html(self):
+    if self.value == None:
+      raise ValueError("All leaf nodes must have a value")
+    if self.tag == None:
+      return self.value
+    else:
+      return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,12 +1,20 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
-class TestTextNode(unittest.TestCase):
+class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
         node = HTMLNode(tag="a", value="link string", props={ "href": "https://www.google.com", "target": "_blank"})
         self.assertEqual(node.__repr__(), "tag: a, value: link string, children: None, props:  href=\"https://www.google.com\" target=\"_blank\"")
+
+class TestLeafNode(unittest.TestCase):
+    def test_repr(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
+
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(node.to_html(), "<p>This is a paragraph of text.</p>")
 
 
 if __name__ == "__main__":
