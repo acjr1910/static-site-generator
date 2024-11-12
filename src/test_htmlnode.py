aa2f77ef6dbd56f65cce67abbfa3f0083932d44c
acjr1910 <acjr1910@gmail.com>
2024-11-12 19:55:45 +0000

    refactor

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,23 +1,78 @@
 import unittest
-
-from htmlnode import HTMLNode, LeafNode, ParentNode
+from htmlnode import LeafNode, ParentNode, HTMLNode
 
 
 class TestHTMLNode(unittest.TestCase):
+    def test_to_html_props(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
+        )
+
+    def test_values(self):
+        node = HTMLNode(
+            "div",
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.tag,
+            "div",
+        )
+        self.assertEqual(
+            node.value,
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.children,
+            None,
+        )
+        self.assertEqual(
+            node.props,
+            None,
+        )
+
     def test_repr(self):
-        node = HTMLNode(tag="a", value="link string", props={ "href": "https://www.google.com", "target": "_blank"})
-        self.assertEqual(node.__repr__(), "tag: a, value: link string, children: None, props:  href=\"https://www.google.com\" target=\"_blank\"")
+        node = HTMLNode(
+            "p",
+            "What a strange world",
+            None,
+            {"class": "primary"},
+        )
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
+        )
 
-class TestLeafNode(unittest.TestCase):
-    def test_to_html(self):
-        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
-        self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
 
-        node = LeafNode("p", "This is a paragraph of text.")
-        self.assertEqual(node.to_html(), "<p>This is a paragraph of text.</p>")
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
-class TestLeafNode(unittest.TestCase):
-    def test_to_html(self):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(),
+                         "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_many_children(self):
         node = ParentNode(
             "p",
             [
@@ -27,34 +82,25 @@ class TestLeafNode(unittest.TestCase):
                 LeafNode(None, "Normal text"),
             ],
         )
-        self.assertEqual(node.to_html(), "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>")
-
-        node = ParentNode(
-            "p",
-            []
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
         )
-        self.assertEqual(node.to_html(), "<p></p>")
 
-
+    def test_headings(self):
         node = ParentNode(
-            "p",
+            "h2",
             [
                 LeafNode("b", "Bold text"),
                 LeafNode(None, "Normal text"),
                 LeafNode("i", "italic text"),
                 LeafNode(None, "Normal text"),
-                ParentNode(
-                    "a",
-                    [
-                        LeafNode("b", "Bold text"),
-                        LeafNode(None, "Normal text"),
-                        LeafNode("i", "italic text"),
-                        LeafNode(None, "Normal text"),
-                    ]
-                ),
             ],
         )
-        self.assertEqual(node.to_html(), "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text<a><b>Bold text</b>Normal text<i>italic text</i>Normal text</a></p>")
+        self.assertEqual(
+            node.to_html(),
+            "<h2><b>Bold text</b>Normal text<i>italic text</i>Normal text</h2>",
+        )
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
-from enum import Enum
 from htmlnode import LeafNode
+from enum import Enum
+
 
 class TextType(Enum):
     TEXT = "text"
@@ -10,30 +11,34 @@ class TextType(Enum):
     IMAGE = "image"
 
 
-class TextNode():
+class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
+        self.text_type = text_type
         self.url = url
-        self.text_type = text_type.value
-
-    def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
     def __eq__(self, other):
-        return self.text == other.text and self.url == other.url and self.text_type == other.text_type
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
 
 def text_node_to_html_node(text_node):
-    if text_node.text_type == TextType.TEXT.value:
+    if text_node.text_type == TextType.TEXT:
         return LeafNode(None, text_node.text)
-    if text_node.text_type == TextType.BOLD.value:
+    if text_node.text_type == TextType.BOLD:
         return LeafNode("b", text_node.text)
-    if text_node.text_type == TextType.ITALIC.value:
+    if text_node.text_type == TextType.ITALIC:
         return LeafNode("i", text_node.text)
-    if text_node.text_type == TextType.CODE.value:
+    if text_node.text_type == TextType.CODE:
         return LeafNode("code", text_node.text)
-    if text_node.text_type == TextType.LINK.value:
+    if text_node.text_type == TextType.LINK:
         return LeafNode("a", text_node.text, {"href": text_node.url})
-    if text_node.text_type == TextType.IMAGE.value:
+    if text_node.text_type == TextType.IMAGE:
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
     raise ValueError(f"Invalid text type: {text_node.text_type}")
